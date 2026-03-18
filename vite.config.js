598f4e9646791550ff09fa66fa680224919f92a2
Drew Floyd <afloyd@chapman.edu>
2026-03-17 21:27:42 -0700

    awards and films sections

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         gallery: resolve(__dirname, 'gallery.html'),
+        films: resolve(__dirname, 'films.html'),
         about: resolve(__dirname, 'about.html'),
+        awards: resolve(__dirname, 'awards.html'),
         contact: resolve(__dirname, 'contact.html'),
       },
     },
